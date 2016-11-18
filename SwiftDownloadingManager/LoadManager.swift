d7802b5169ff904a7275446c2a2ec39ee88f73d9
ToxaZhe <muroslava.m.2012@gmail.com>
2016-11-19 00:46:58 +0200

    add download metrics, start/stop URLSessionDataTask

--- SwiftDownloadingManager/LoadManager.swift
+++ SwiftDownloadingManager/LoadManager.swift
@@ -9,13 +9,15 @@
 import Foundation
 
 
-class LoadManager: NSObject, URLSessionDataDelegate {
+class LoadManager: NSObject, URLSessionDataDelegate, URLSessionDelegate, URLSessionTaskDelegate {
     
+    
+    let metrics = URLSessionTaskMetrics()
     var _fileUrl: String?
-    var _fileTask: URLSessionDataTask?
+    var _fileTask: URLSessionTask?
     var _fileDowloaded = false
     var _expectedFileLenght: Int64?
-    var _fileData: Data?
+    var _fileData = Data()
     
     
     func loadFileFromUrl(urlString: String) {
@@ -23,36 +25,58 @@ class LoadManager: NSObject, URLSessionDataDelegate {
         let url = URL(string: urlString)!
         let defConfigObject = URLSessionConfiguration.default
         let defaultSession = URLSession(configuration: defConfigObject, delegate: self, delegateQueue: OperationQueue.main)
-        _fileTask = defaultSession.dataTask(with: url, completionHandler: { (data, response, error) in
-            if (error != nil) {
-                print(error.debugDescription)
-            } else {
-                self._fileTask?.resume()
-                self._expectedFileLenght = response?.expectedContentLength
-                self._fileData = data
-                print((self._fileData?.description)! as String)
-//                let lenght = self._fileData?.count
-//MARK: add CoreData startingDate
-            }
-        })
+        _fileTask = defaultSession.dataTask(with: url)
+        self._fileTask?.resume()
     }
+    
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
+        if dataTask == _fileTask {
+            _expectedFileLenght = response.expectedContentLength
+            print("\(_expectedFileLenght)")
+            completionHandler(.allow)
+        }
+    
+    }
+
    
     func urlSession(_ session: URLSession, task: URLSessionTask, didFinishCollecting metrics: URLSessionTaskMetrics) {
         if task == _fileTask {
+            print("\(task.countOfBytesExpectedToReceive) - \(task.countOfBytesReceived) ")
             let startDate = metrics.taskInterval.start
             let finishedDate = metrics.taskInterval.end
             print("\(startDate) - \(finishedDate)")
-            
         }
-        
     }
     
     
-//   func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
-//        
-//    }
-//    
-//   func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
-//    
-//    }
+   func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        if dataTask == _fileTask {
+            _fileData.append(data)
+            print("\(_fileData.count)")
+        }
+    }
+    
+    
+    func start() {
+        _fileTask?.resume()
+    }
+    
+    func stop() {
+        _fileTask?.suspend()
+    }
+    
+    
+    func stopResumeDowload() {
+        if _fileTask?.state == .running {
+            stop()
+        } else {
+            start()
+        }
+    }
+    
+    
+    
+    
+    
+    
 }
