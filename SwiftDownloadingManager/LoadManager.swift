c8d6b83b455f58dfa670c97979f883fd820535bd
ToxaZhe <bizconsulting.sm2@yandex.ru>
2016-11-25 11:48:06 +0200

    add FileManager save file  to LoadMaanager

--- SwiftDownloadingManager/LoadManager.swift
+++ SwiftDownloadingManager/LoadManager.swift
@@ -8,6 +8,9 @@
 
 import Foundation
 
+protocol LoadManagerInfoDelegate: class {
+    func getLoadingInfo(_ startDate: Date?, finishedDate: Date?, downloaded: Bool, progress: Double?, error: Error?)
+}
 
 class LoadManager: NSObject, URLSessionDataDelegate, URLSessionDelegate, URLSessionTaskDelegate {
     
@@ -18,6 +21,10 @@ class LoadManager: NSObject, URLSessionDataDelegate, URLSessionDelegate, URLSess
     var _fileDowloaded = false
     var _expectedFileLenght: Int64?
     var _fileData = Data()
+    var _error: Error?
+    var _startDowloading: Date?
+    var _finishDowloading: Date?
+    weak var delegate: LoadManagerInfoDelegate?
     
     
     func loadFileFromUrl(urlString: String) {
@@ -37,22 +44,43 @@ class LoadManager: NSObject, URLSessionDataDelegate, URLSessionDelegate, URLSess
         }
     
     }
+    
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        if error != nil {
+            delegate?.getLoadingInfo(nil, finishedDate: nil, downloaded: true, progress: nil, error: error)
+        } else {
+            guard let url = _fileUrl else {print("LoadManager -> can't save dat - no urlString"); return}
+            let fileName = FileManage.getFileName(urlString: url)
+            FileManage.saveMp3ToTheAppDirectory(fileData: _fileData, fileName: fileName, onSuccess: { 
+                delegate?.getLoadingInfo(_startDowloading, finishedDate: _finishDowloading, downloaded: true, progress: Double(_expectedFileLenght!), error: nil)
+            }, onError: { (failure) in
+                delegate?.getLoadingInfo(nil, finishedDate: nil, downloaded: true, progress: nil, error: failure)
+            })
+            
+        }
+    }
 
    
     func urlSession(_ session: URLSession, task: URLSessionTask, didFinishCollecting metrics: URLSessionTaskMetrics) {
         if task == _fileTask {
             print("\(task.countOfBytesExpectedToReceive) - \(task.countOfBytesReceived) ")
-            let startDate = metrics.taskInterval.start
-            let finishedDate = metrics.taskInterval.end
-            print("\(startDate) - \(finishedDate)")
+            _startDowloading = metrics.taskInterval.start
+            _finishDowloading = metrics.taskInterval.end
+            print("\(_startDowloading) - \(_finishDowloading)")
         }
     }
     
+    func urlSession(_ session: URLSession, task: URLSessionTask, didSendBodyData bytesSent: Int64, totalBytesSent: Int64, totalBytesExpectedToSend: Int64) {
+        
+        let uploadProgress = Double(totalBytesSent) / Double(totalBytesExpectedToSend) * 100
+        
+        delegate?.getLoadingInfo(nil, finishedDate: nil, downloaded: false, progress: uploadProgress, error: nil)
+    }
     
    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
         if dataTask == _fileTask {
             _fileData.append(data)
-            print("\(_fileData.count)")
+    
         }
     }
     
@@ -69,12 +97,16 @@ class LoadManager: NSObject, URLSessionDataDelegate, URLSessionDelegate, URLSess
     func stopResumeDowload() {
         if _fileTask?.state == .running {
             stop()
-        } else {
+        } else if _fileTask?.state == .suspended {
             start()
+        } else if _fileTask?.state == .completed {
+            print("_fileTask download completed")
+            _fileDowloaded = true
         }
     }
     
     
+  
     
     
     
